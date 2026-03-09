87083088091bdf791348aee39e6a60b910821f03
KiniRami <ramikini007@gmail.com>
2026-03-09 20:59:04 +0100

    1Mainclient.java

--- src/Mainclient.java
+++ src/Mainclient.java
@@ -1,16 +1,8 @@
 public class Mainclient {
     public static void main(String[] args) {
         Client client = new Client("Rami", "blu");
-
-
-         int c=client.connetti("localhost", 12345);
-
-
-        if (c == 0) {
-            client.scrivi();
-            client.leggi();
-        } else {
-            System.out.println("Impossibile connettersi al server.");
+        if (client.connetti("localhost", 12345) == 0) {
+            client.comunica();
         }
     }
 }
